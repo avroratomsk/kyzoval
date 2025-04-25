a36ca9150324e088c60f5f57d4e11f1af4c3a60f
sanchyopas <saniagolovanev@gmail.com>
2025-04-25 10:24:14 +0700

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,13 @@ import * as functions from './modules/baseFunctions.js';
 
 functions.isWebp();
 
-// new VenoBox({
-//   selector: '.my-image-links',
-//   numeration: true,
-//   infinigall: true,
-//   share: true,
-//   spinner: 'rotating-plane'
-// });
+new VenoBox({
+  selector: '.my-image-links',
+  numeration: true,
+  infinigall: true,
+  share: true,
+  spinner: 'rotating-plane'
+});
 
 const navItemElem = document.querySelectorAll('.nav__item');
 
